f7f135faf5eceb04dcfaa9015dde38c81bddd0f8
Rhys Hodgson <121772484+rhysez@users.noreply.github.com>
2025-07-20 14:10:35 +0100

    Add basic implementation of traverse method which reads files at the specified path.

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,13 @@ use crate::visualiser::traverser::TreeTraverser;
 pub mod visualiser;
 
 fn main() {
-    let origin = String::from("~/");
+    let origin_path = String::from("./");
 
     let tree_traverser = TreeTraverser {
-        origin_path: &origin,
+        origin_path: &origin_path,
         max_traversal_depth: 2,
         current_traversal_depth: 1,
+        accumulative_file_count: 0,
     };
 
     tree_traverser.traverse();
--- src/visualiser/traverser.rs
+++ src/visualiser/traverser.rs
@@ -1,19 +1,22 @@
+use std::fs;
+
 // Using string slices so that struct does not take ownership.
 // Also passing generic lifetime that origin_path and current_file_name share.
 pub struct TreeTraverser<'a> {
     pub origin_path: &'a str, // The starting path that Willow starts traversing from.
     pub max_traversal_depth: u32, // The maximum traversal depth allowed.
     pub current_traversal_depth: u32, // The current depth of the traversal at some point in time.
+    pub accumulative_file_count: u32, // The total number of files found thus far.
 }
 
 // TO DO
 // 1. Iterate through wd and print files.
-
+// 2. Return file count to main function.
 impl<'a> TreeTraverser<'a> {
     pub fn traverse(&self) {
-        // Starts with first file in origin path.
-        //let mut current_file: String = String::from("");
-
-        println!("Traversing from {}", self.origin_path);
+        let entries = fs::read_dir(self.origin_path).unwrap();
+        for entry in entries {
+            println!("{}", entry.unwrap().path().display())
+        }
     }
 }
